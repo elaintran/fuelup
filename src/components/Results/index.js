c3fd6066dd909d70e8c6f7a9af7c5fcb659a8894
elaintran <elain.ttran@gmail.com>
2019-09-12 13:58:02 -0500

    restructured code; passed search result as parameter to result component

---
+++ src/components/Results/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.sass";
+
+function Results(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Results;
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,50 +1,12 @@
-import React, { Component } from "react";
-import API from "../../utils/API.js";
-import cheerio from "cheerio";
+import React from "react";
 import "./style.sass";
 
-class SearchBar extends Component {
-    state = {
-        query: "",
-        results: [],
-        search: false
-    }
-
-    handleInput = event => {
-        let value = event.target.value;
-        this.setState({ query: value });
-    }
-
-    handleSubmit = event => {
-        event.preventDefault();
-        this.searchPrices(this.state.query);
-    }
-
-    searchPrices = query => {
-        API.findPrices(query).then(response => {
-            const $ = cheerio.load(response.data);
-            const resultArr = [];
-            $(".GenericStationListItem__stationListItem___3Jmn4").each((i, element)=> {
-                const result = {};
-                result.station = $(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".header__header___1zII0").text();
-                result.logo = $(element).children(".GenericStationListItem__logoColumn___2oYl7").children(".StationBrandings__logoImages___1KnoP").children(".GenericStationListItem__logoImageContainer___2SVFN").children(".image__image___1ZUby").attr("src");
-                result.address = $(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".GenericStationListItem__address___1VFQ3").html().replace("<br>", ", ");
-                result.price = $(element).children(".GenericStationListItem__priceColumn___UmzZ7").children(".GenericStationListItem__priceContainer___YbVoO").children("span").text();
-                result.lastUpdated = $(element).children(".GenericStationListItem__priceColumn___UmzZ7").children(".GenericStationListItem__priceContainer___YbVoO").children("div").children(".ReportedBy__postedTime___J5H9Z").text();
-                resultArr.push(result);
-            })
-            this.setState({ results: resultArr});
-            console.log(this.state.results);
-        });
-    }
-
-    render() {
-        return (
-            <form className="search" onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="Search gas prices by city or zipcode..." onChange={this.handleInput} required />
-            </form>
-        );
-    }
+function SearchBar(props) {
+    return (
+        <form className="search" onSubmit={props.submit}>
+            <input type="text" placeholder="Search gas prices by city or zipcode..." onChange={props.change} required />
+        </form>
+    );
 }
 
 export default SearchBar;
