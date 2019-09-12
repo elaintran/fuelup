fb85bc49faefa641029cd25b0a9bf11bc002f273
elaintran <elain.ttran@gmail.com>
2019-09-11 22:28:18 -0500

    scraped gas station info

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -23,8 +23,18 @@ class SearchBar extends Component {
     searchPrices = query => {
         API.findPrices(query).then(response => {
             const $ = cheerio.load(response.data);
-            let prices = $(".GenericStationListItem__price___3GpKP").text();
-            console.log(prices);
+            const resultArr = [];
+            $(".GenericStationListItem__stationListItem___3Jmn4").each((i, element)=> {
+                const result = {};
+                result.station = $(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".header__header___1zII0").text();
+                result.logo = $(element).children(".GenericStationListItem__logoColumn___2oYl7").children(".StationBrandings__logoImages___1KnoP").children(".GenericStationListItem__logoImageContainer___2SVFN").children(".image__image___1ZUby").attr("src");
+                result.address = $(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".GenericStationListItem__address___1VFQ3").html().replace("<br>", ", ");
+                result.price = $(element).children(".GenericStationListItem__priceColumn___UmzZ7").children(".GenericStationListItem__priceContainer___YbVoO").children("span").text();
+                result.lastUpdated = $(element).children(".GenericStationListItem__priceColumn___UmzZ7").children(".GenericStationListItem__priceContainer___YbVoO").children("div").children(".ReportedBy__postedTime___J5H9Z").text();
+                resultArr.push(result);
+            })
+            this.setState({ results: resultArr});
+            console.log(this.state.results);
         });
     }
 
