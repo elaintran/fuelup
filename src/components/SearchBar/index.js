af7ffcb6762e7a8eb09f26fa5d9a125c79b9cb25
elaintran <elain.ttran@gmail.com>
2019-09-11 21:49:55 -0500

    fixed cors issue in api call

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import API from "../../utils/API.js";
 import "./style.sass";
 
 class SearchBar extends Component {
@@ -13,9 +14,20 @@ class SearchBar extends Component {
         this.setState({ query: value });
     }
 
+    handleSubmit = event => {
+        event.preventDefault();
+        this.searchPrices(this.state.query);
+    }
+
+    searchPrices = query => {
+        API.findPrices(query).then(response => {
+            console.log(response.data);
+        });
+    }
+
     render() {
         return (
-            <form className="search">
+            <form className="search" onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="Search gas prices by city or zipcode..." onChange={this.handleInput} required />
             </form>
         );
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default {
-    findGasPrices: query => {
-        return axios.get(`https://www.gasbuddy.com/home?search=${query}&fuel=1`);
+    findPrices: query => {
+        return axios.get(`https://cors-anywhere.herokuapp.com/https://www.gasbuddy.com/home?search=${query}&fuel=1`);
     }
 };
