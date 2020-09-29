e1f974d5c34498abfb80196e282fd50007ec8ac6
elaintran <elain.ttran@gmail.com>
2020-09-29 14:15:58 -0500

    updated gas info targeting on backend

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
+const jsdom = require("jsdom");
 const userController = require("../../controllers/userController.js");
 const stationController = require("../../controllers/stationController.js");
 const passport = require("../../config/passport.js");
@@ -9,13 +10,13 @@ const passport = require("../../config/passport.js");
 router.get("/gasbuddy/:id", (req, res) => {
     axios.get(`https://www.gasbuddy.com/home?search=${req.params.id}&fuel=1`)
         .then(response => {
-            const $ = cheerio.load(response.data);
+            const $ = cheerio.load(response.data, {xmlMode: true});
             const resultArr = [];
             $(".GenericStationListItem__stationListItem___3Jmn4").each((i, element) => {
                 const result = {};
                 result.station = $(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".header__header___1zII0").text().trim();
-                result.logo = $(element).children(".GenericStationListItem__logoColumn___2oYl7").children(".StationBrandings__logoImages___1KnoP").children(".GenericStationListItem__logoImageContainer___2SVFN").children(".image__image___1ZUby").attr("src");
-                result.address = $(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".GenericStationListItem__address___1VFQ3").html().replace("<br>", ", ");
+                result.logo = $(element).children(".GenericStationListItem__logoColumn___2oYl7").children(".StationBrandings__logoImages___1KnoP").children(".GenericStationListItem__logoImageContainer___2SVFN").children("noscript").children(".image__image___1ZUby").attr("src");
+                result.address = $(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".GenericStationListItem__address___1VFQ3").html().replace("<br/>", ", ");
                 result.link = `https://www.gasbuddy.com${$(element).children(".GenericStationListItem__mainInfoColumn___2kuPq").children(".header__header___1zII0").children("a").attr("href")}`;
                 resultArr.push(result);
             });
